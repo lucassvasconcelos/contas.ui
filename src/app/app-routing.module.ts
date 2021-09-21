aee335320c1dfca33a6a354e3f53282287ca2ef9
Lucas Vasconcelos <lucasv1992@gmail.com>
2021-09-21 01:37:56 -0300

    cria template basico com menu e footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { ContasComponent } from './pages/contas/contas.component';
+import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'relatorios', pathMatch: 'full' },
+  { path: 'relatorios', component: RelatoriosComponent },
+  { path: 'contas', component: ContasComponent },
+  { path: 'categorias', component: CategoriasComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'ignore', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContasComponent } from './pages/contas/contas.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContasComponent,
+    CategoriasComponent,
+    RelatoriosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
