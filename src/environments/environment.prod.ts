71906de44d8f9fa064a43fd90771e45455a41bef
Lucas Vasconcelos <lucasv1992@gmail.com>
2021-10-28 01:13:01 -0300

    +semver:feature adiciona task deploy para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  serviceUrl: 'http://localhost:5000',
+  production: true,
+  serviceUrl: 'https://contasapi.herokuapp.com',
   fakeUser: {
     id: "30c8a3fc-2b04-4713-802a-a62f1f519bea",
     name: "Lucas Vasconcelos"
